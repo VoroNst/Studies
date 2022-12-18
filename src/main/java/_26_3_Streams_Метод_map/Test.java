8a53631f7bb884b5d3556aec6165a1dd18ae9c91
Valery <voronst1@mail.ru>
2022-12-18 17:53:45 +0300

    ok 1

--- src/main/java/_26_3_Streams_Метод_map/Test.java
+++ src/main/java/_26_3_Streams_Метод_map/Test.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Test {
-
-
     public static void main(String[] args) {
         List<String> stringList = new ArrayList<>();
         stringList.add("Privet");
@@ -13,18 +11,27 @@ public class Test {
         stringList.add("Da");
         stringList.add("Net");
 
+        // wordLength1(stringList);
+
+        wordLength2(stringList);
+    }
+
+    static void wordLength1(List<String> stringList) {
         for (int i = 0; i < stringList.size(); i++) {
+            System.out.print(stringList.get(i) + " ");
             stringList.set(i, String.valueOf(stringList.get(i).length()));
+            System.out.print(stringList.get(i) + " ");
         }
+        System.out.println("\n" + stringList);
+    }
 
-
+    static void wordLength2(List<String> stringList) {
         int i = 0;
         for (String s : stringList) {
-
+            System.out.print(s + " ");
             stringList.set(i++, String.valueOf(s.length()));
-
         }
-
-        System.out.println(stringList);
+        System.out.println("\n" + stringList);
     }
 }
+
